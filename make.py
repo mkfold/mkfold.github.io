b4b6b6f05824d5312a676f44696bef048b47b5b3
mia <15945773+mkfold@users.noreply.github.com>
2023-09-05 22:19:16 -0700

    everything except templates goes into src dir now added copy fn for extra files in src dir/subdirs more code cleaning removed pointless verbose mode

--- make.py
+++ make.py
@@ -69,13 +69,23 @@ def generate_toc(page_info):
     return TOFC_TEMPLATE.render({"toc": toc} if len(toc) else {})
 
 
-def save_page(html, html_path, id_=None):
-    fdir = path.join(html_path, id_) if id_ is not None else html_path
-    outpath = path.join(fdir, "index.html")
-
-    os.makedirs(fdir, exist_ok=True)
-    with open(outpath, "w") as f:
+def save_html(html, html_path):
+    os.makedirs(html_path, exist_ok=True)
+    dst_path = path.join(html_path, "index.html")
+    with open(dst_path, "w") as f:
         f.write(html)
+
+
+def copy_extra_files(md_path, html_path):
+    src_paths = [
+        x for x in glob(path.join(md_path, "*"))
+        if not x.endswith("index.md") and not path.isdir(x)
+    ]
+    dst_paths = [
+        path.join(html_path, x.removeprefix(md_path)[1:]) for x in src_paths
+    ]
+    for src, dst in zip(src_paths, dst_paths):
+        shutil.copyfile(src, dst)
 
 
 def make_pages(docs, html_path, force_update=False):
@@ -89,14 +99,15 @@ def make_pages(docs, html_path, force_update=False):
 
         html, props = generate_page(text)
         new_props[id_] = PageInfo(id_, h, **props)
-        save_page(html, html_path, id_=id_)
 
+        dst_path = path.join(html_path, id_)
+        save_html(html, dst_path)
     return new_props
 
 
-def del_pages(keys, html_path):
-    for k in keys:
-        if path.exists(p := path.join(html_path, k)):
+def del_pages(ids, html_path):
+    for i in ids:
+        if path.exists(p := path.join(html_path, i)):
             shutil.rmtree(p)
         else:
             print(f'warning: did not find directory "{p}" to delete')
@@ -110,34 +121,31 @@ def load_manifest(f):
     return [PageInfo(*v) for v in csv.reader(f)]
 
 
-def make(manifest_path, md_path, html_path, force_update=False, verbose=False):
-    vprint = print if verbose else bool  # hehe
+def make(manifest_path, md_path, html_path, copy_extras=False, force=False):
+    manifest_path = path.normpath(manifest_path)
+    md_path = path.normpath(md_path)
+    html_path = path.normpath(html_path)
 
     def path2id(x):
-        return path.splitext(path.basename(x))[0]
+        return path.basename(path.dirname(x))
 
     def id2path(x):
-        return path.join(md_path, f"{x}.md")
+        return path.join(md_path, x, "index.md")
 
     if path.exists(manifest_path):
         with open(manifest_path, "r", newline="") as f:
             manif = {x.id_: x for x in load_manifest(f)}
-        vprint(f'{len(manif)} pages(s) in "{manifest_path}"')
+        print(f"loaded manifest with {len(manif)} post(s)")
     else:
         print("warning: no manifest found. creating empty manifest")
         manif = {}
 
-    md_files = glob(path.join(md_path, "*.md"))
+    md_files = glob(path.join(md_path, "*", "index.md"))
     html_files = glob(path.join(html_path, "*", "index.html"))
 
     mf_ids = set(manif)
     md_ids = {path2id(x) for x in md_files}
-    html_ids = {path2id(path.split(x)[0]) for x in html_files}
-
-    vprint(
-        f'{len(md_ids)} md files(s) in "{md_path}"\n'
-        f'{len(html_ids)} html files(s) in "{html_path}"'
-    )
+    html_ids = {path2id(x) for x in html_files}
 
     matched_i = mf_ids & html_ids  # matched: has mf entry and html
     del_i = mf_ids - md_ids        # deleted: any mf entry not in md dir
@@ -156,17 +164,17 @@ def make(manifest_path, md_path, html_path, force_update=False, verbose=False):
     print(f":: {len(new_data)} new page(s) generated")
 
     upd_data = (
-        make_pages(x, html_path, force_update)
+        make_pages(x, html_path, force)
         if len(x := [(k, id2path(k), manif[k].hash_) for k in upd_i]) else {}
     )
     print(f":: {len(upd_data)} page(s) updated")
 
-    if len(deleted := del_i & html_ids):
-        del_pages(deleted, html_path)
-        print(f":: {len(deleted)} page(s) deleted from disk")
+    if len(to_delete := del_i & html_ids):
+        del_pages(to_delete, html_path)
+    print(f":: {len(to_delete)} page(s) deleted from disk")
 
     upd_data |= new_data
-    if force_update or len(del_i) or len(upd_data):
+    if force or len(del_i) or len(upd_data):
         manif = {k: x for k, x in manif.items() if k not in del_i}
         manif |= upd_data
         toc_data = [x for x in manif.values() if x.date is not None]
@@ -175,12 +183,17 @@ def make(manifest_path, md_path, html_path, force_update=False, verbose=False):
             print(f":: {len(del_i)} page(s) dropped from manifest")
 
         toc_html = generate_toc(toc_data)
-        save_page(toc_html, html_path)
+        save_html(toc_html, html_path)
         print(f":: index with {len(toc_data)} post(s) generated")
 
         with open(manifest_path, "w", newline="") as f:
             save_manifest(f, manif.values())
         print(":: updated manifest")
+
+    if copy_extras:
+        copy_extra_files(md_path, html_path)
+        for i in upd_data:
+            copy_extra_files(path.dirname(id2path(i)), path.join(html_path, i))
 
     print("done")
 
@@ -191,7 +204,7 @@ if __name__ == "__main__":
     p.add_argument("--manifest-path", type=str, default=DEFAULT_MANIFEST_PATH)
     p.add_argument("--md-path", type=str, default=DEFAULT_CONTENT_PATH)
     p.add_argument("--html-path", type=str, default=DEFAULT_PUBLIC_PATH)
-    p.add_argument("--force-update", "-f", action="store_true")
-    p.add_argument("--verbose", "-v", action="store_true")
+    p.add_argument("--copy-extras", "-c", action="store_true")
+    p.add_argument("--force", "-f", action="store_true")
 
     make(**vars(p.parse_args()))
